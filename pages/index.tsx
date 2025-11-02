e76c53fa29ed84f94e3944a038f6db4339701188
Nicholas <contactnicholas81@gmail.com>
2025-11-02 10:04:02 +0000

    make some ui changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} key="desc" />
-        <meta name="theme-color" content="#f54bff" />
+        <meta name="theme-color" content="#b921ff" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="twitter:title" content={title} />
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
       <Script async defer src="https://buttons.github.io/buttons.js" />
       <Sidebar picture={picture} opened={opened} close={close} />
       <h1 className="font-bold text-5xl tracking-tight">Pics</h1>
-      <p className="mt-2 text-gray-500">{description}</p>
+      <p className="mt-4 text-lg text-gray-500">{description}</p>
       <PictureList selected={selected} close={close} />
       <Footer />
     </Layout>
